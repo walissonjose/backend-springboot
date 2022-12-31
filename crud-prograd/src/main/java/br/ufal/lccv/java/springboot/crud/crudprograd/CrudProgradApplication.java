899391f8ccf372b41542f5c5c7aac9901593c249
Walisson Araujo <walisson.araujo@ctec.ufal.br>
2022-12-31 12:13:01 -0300

    projeto feito

---
+++ crud-prograd/src/main/java/br/ufal/lccv/java/springboot/crud/crudprograd/CrudProgradApplication.java
@@ -0,0 +1,13 @@
+package br.ufal.lccv.java.springboot.crud.crudprograd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProgradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProgradApplication.class, args);
+	}
+
+}
---
+++ crud-prograd/src/test/java/br/ufal/lccv/java/springboot/crud/crudprograd/CrudProgradApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufal.lccv.java.springboot.crud.crudprograd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProgradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
